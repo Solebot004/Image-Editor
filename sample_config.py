1e1fa8abb95a200852871928113b63d321642b9e
Solebots <142100943+Solebots@users.noreply.github.com>
2023-08-12 19:35:35 +0530

    sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,11 +5,11 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("6671599645:AAEogUro_tkobCzijcsI36GTzm_oV5lU6NY", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = os.environ.get("22923523", "")
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("d52c7824d0e66903a0724b800a16ce2c", "")
 
     # Get this api from https://www.remove.bg/b/background-removal-api
-    RemoveBG_API = os.environ.get("RemoveBG_API", "")
+    RemoveBG_API = os.environ.get("vCuhnUhz5vrqbEakzKujQHpg", "")
