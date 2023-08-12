43f35bece48e14b3971d24840187df04c7689da4
Solebots <142100943+Solebots@users.noreply.github.com>
2023-08-12 19:34:22 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,17 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config  # pylint:disable=import-error
+    from sample_config import Config  # pylint:disable=import-error
 
 
 if __name__ == "__main__":
     plugins = dict(root="plugins")
 
     app = Client(
-        "TroJanz",
-        bot_token=Config.TG_BOT_TOKEN,
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
+        "lisa",
+        bot_token='6671599645:AAEogUro_tkobCzijcsI36GTzm_oV5lU6NY',
+        api_id='22923523',
+        api_hash='d52c7824d0e66903a0724b800a16ce2c',
         plugins=plugins,
         workers=300,
     )
