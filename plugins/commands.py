fb9632fd5791168f6b120edfee5c866ea46e7c5d
Solebots <142100943+Solebots@users.noreply.github.com>
2023-08-12 19:39:20 +0530

    commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,8 @@ async def start(client: Client, message: Message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "DEVELOPER",
+                            url="https://t.me/sole_bots",
                         )
                     ],
                 ]
@@ -44,8 +44,8 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "DEVELOPER",
+                            url="https://t.me/sole_bots",
                         )
                     ],
                 ]
@@ -70,8 +70,8 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "DEVELOPER",
+                            url="https://t.me/sole_bots",
                         )
                     ],
                 ]
